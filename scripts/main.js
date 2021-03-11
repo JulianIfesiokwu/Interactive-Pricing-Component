5d9f5afe22146910ee81e7bd67d10e1c9426d40b
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2021-03-11 18:15:56 +0100

    completed range slider background color, and started working on billing toggle button

--- scripts/main.js
+++ scripts/main.js
@@ -2,33 +2,47 @@ const slider = document.getElementById('myRange');
 const price = document.getElementById('price');
 const pageViews = document.getElementById('page-views');
 
-//displays the default value
+function updateGradient(rangeValue) {
+    const percentage = (rangeValue - slider.min) / (slider.max - slider.min) * 100;
+    slider.style.backgroundImage = `linear-gradient(90deg, hsl(174, 86%, 45%) ${percentage}%, transparent ${percentage}%)`;
+}
 
-// Update the current slider value (each time you drag the slider handle)
+// Update the current price,pageviews and slider gradient (each time you drag the slider handle)
 slider.oninput = function() {    
     switch (slider.value) {
         case '1' :
             pageViews.textContent = `${10}K PAGEVIEWS`;
-            price.textContent = '$8';        
+            price.textContent = '$8.00';
+            updateGradient(slider.value);      
             break;
         case '2' :
             pageViews.textContent = `${50}K PAGEVIEWS`;
-            price.textContent = `$12`;           
+            price.textContent = `$12.00`;
+            updateGradient(slider.value);           
             break;
         case '3' :
             pageViews.textContent = `${100}K PAGEVIEWS`;
-            price.textContent = `$16`;         
+            price.textContent = `$16.00`;
+            updateGradient(slider.value);          
             break;
         case '4' :
             pageViews.textContent = `${500}K PAGEVIEWS`; 
-            price.textContent = `$24`           
+            price.textContent = `$24.00`;
+            updateGradient(slider.value);           
             break;
         case '5' :
             pageViews.textContent = `${1}M PAGEVIEWS`;
-            price.textContent = `$36`            
+            price.textContent = `$36.00`;
+            updateGradient(slider.value);             
             break;
 
         default:
     }
 }
-  price.innerHTML = `$12`;
+
+//displays the default slider value on load
+price.innerHTML = `$16.00`;
+
+//update slider gradient on load
+updateGradient(slider.value);
+  
