11235189016ff4715e68fe0b12725a38a2f27c19
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2021-06-02 11:26:49 +0100

    I have completed alot of the functionality using javascript,but the checkbox still having problems,I also updated the read-me as per guidelines by FrontEnd Mentor

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# Interactive-Pricing-Component
-A project using HTML,CSS and JS to create a billing pricing component. Sourced from FrontendMentor.io
+# Frontend Mentor - Coding bootcamp testimonials slider solution
+
+This is a solution to the [Coding bootcamp testimonials slider challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/coding-bootcamp-testimonials-slider-4FNyLA8JL). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- HTML5 markup
+- CSS
+- Flexbox
+
+### What I learned
+I learned about different media queries and how they affect mobile view at different scrren sizes. I am really proud of my media quewries for tablets
+@media only screen and (min-width: 600px)and (max-width: 768px) {...}
+
+
+I also learnt about changing displayed HTML based on screen sizes using javascript. w3schools was particularly useful here.See the js code below. I never knew about this and I was so impressed with the result.
+
+// Change discount text when viewport size changes
+function handleViewportChange(mql) {
+    if(mql.matches) {
+        discount.textContent = `-25%`;
+    } else {
+        discount.textContent = `-25% discount`;
+    }
+}
+
+### Continued development
+
+I want to keep improving my js as I had problems with the checkbox and making it revert back to standard prices. I decided to hard code it and will change this later if I can think of a better solution.
+
+### Useful resources
+
+https://www.w3schools.com/
+
+## Author
+
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/egbuna09)
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ const discount = document.getElementById('discount');
 const mql = window.matchMedia('(max-width: 600px)');
 const discountCheckbox = document.getElementById('checkbox');
 
+// Change discount text when viewport size changes
 function handleViewportChange(mql) {
     if(mql.matches) {
         discount.textContent = `-25%`;
@@ -14,46 +15,51 @@ function handleViewportChange(mql) {
     }
 }
 
+// Apply gradient when slider thumb moves
 function updateGradient(rangeValue) {
     const percentage = (rangeValue - slider.min) / (slider.max - slider.min) * 100;
     slider.style.backgroundImage = `linear-gradient(90deg, hsl(174, 86%, 45%) ${percentage}%, transparent ${percentage}%)`;
 }
 
-// function discountPrices(price) {
-//     if(discountCheckbox.checked) {
-//         price.textContent = `${}`
-//     } else {
-//         //let discountedPrice = `${price.textContent.slice(1)} - ${(price.textContent.slice(1) / 4)}`;
-//         console.log(price.textContent)
-//     }    
-// }
+// Apply discount to prices
+function discountPrice() {
+    let currentPrice = price.textContent;
+    console.log(parseFloat(currentPrice.slice(1)).toFixed(2))
+
+    if(discountCheckbox.checked) {
+        price.textContent = `$${(parseFloat(currentPrice.slice(1))).toFixed(2) * 3 / 4}`;
+    }
+}
+
 
 // Update the current price,pageviews and slider gradient (each time you drag the slider handle)
-slider.oninput = function() {    
+slider.oninput = function() {
+    
     switch (slider.value) {
         case '1' :
             pageViews.textContent = `${10}K PAGEVIEWS`;
-            price.textContent = '$8.00';
+            // updatePrices(slider.value)
+            discountCheckbox.checked ? price.textContent = `$${8.00 * 3 / 4}` : price.textContent = `$8.00`;
             updateGradient(slider.value);    
             break;
         case '2' :
             pageViews.textContent = `${50}K PAGEVIEWS`;
-            price.textContent = `$12.00`;
+            discountCheckbox.checked ? price.textContent = `$${12.00 * 3 / 4}` : price.textContent = `$12.00`;
             updateGradient(slider.value);           
             break;
         case '3' :
             pageViews.textContent = `${100}K PAGEVIEWS`;
-            price.textContent = `$16.00`;
+            discountCheckbox.checked ? price.textContent = `$${16.00 * 3 / 4}` : price.textContent = `$16.00`;
             updateGradient(slider.value);          
             break;
         case '4' :
             pageViews.textContent = `${500}K PAGEVIEWS`; 
-            price.textContent = `$24.00`;
+            discountCheckbox.checked ? price.textContent = `$${24.00 * 3 / 4}` : price.textContent = `$24.00`;
             updateGradient(slider.value);           
             break;
         case '5' :
             pageViews.textContent = `${1}M PAGEVIEWS`;
-            price.textContent = `$36.00`;
+            discountCheckbox.checked ? price.textContent = `$${36.00 * 3 / 4}` : price.textContent = `$36.00`;
             updateGradient(slider.value);             
             break;
 
@@ -74,4 +80,4 @@ handleViewportChange(mql);
 
 mql.addListener(handleViewportChange);
   
-// discountCheckbox.addEventListener('click', discountPrices);
+discountCheckbox.addEventListener('click', discountPrice);
